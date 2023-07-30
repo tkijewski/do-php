97951d32c2c627774cad15c86fc4d0302cfb8d01
Tim K <tim.kijewski@gmail.com>
2023-07-30 08:56:11 -0400

    cleanup

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace app\controllers\backend;
+namespace app\controllers;
 
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\backend\LoginForm;
-use app\models\backend\ContactForm;
+use app\models\LoginForm;
+use app\models\ContactForm;
 
 class SiteController extends Controller
 {
